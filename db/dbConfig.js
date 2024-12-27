1605eecd1df02214af312c314d3354e577cca951
sathiska sasindu <122218622+sathiska567@users.noreply.github.com>
2024-12-27 10:20:52 +0530

    add

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,19 +1,24 @@
 const sql = require('mssql');
 
+
 const config = {
+  
     database: 'pos',
     server: 'LAPTOP-VEQSQ0JG',
+
     options: {
       trustedConnection: true, // Enables Windows Authentication
       encrypt: false, // Disable encryption for local development
       trustServerCertificate: true, // Required for self-signed certificates
     },
+
     pool: {
       max: 10,
       min: 0,
       idleTimeoutMillis: 30000,
     },
 };
+
 
 const poolPromise = new sql.ConnectionPool(config)
   .connect()
